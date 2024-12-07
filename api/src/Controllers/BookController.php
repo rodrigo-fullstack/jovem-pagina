53668535b0987703652e05edb2261264bd8fd9dc
rodrigo-backend <rodrigopereirafaria9@gmail.com>
2024-12-07 13:48:04 -0300

    feat (register): Cadastrando livros com autenticação do vendedor

---
+++ api/src/Controllers/BookController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Http\Request;
+use App\Http\Response;
+use App\Services\BookService;
+use App\Services\UserService;
+
+class BookController{
+
+    public function save(Request $request, Response $response){
+        $body = $request::body();
+
+        $jwt = $request::authorization();
+
+        $bookServ = BookService::save($body, $jwt);
+
+        if(isset($bookServ['unauthorized'])){
+            return $response::json([
+                "error" => true,
+                "success" => false,
+                "message" => $bookServ['unauthorized']
+            ], 401);
+        }
+
+        if(isset($bookServ['error'])){
+            return $response::json([
+                "error" => true,
+                "success" => false,
+                "message" => $bookServ['error']
+            ], 400);
+        }
+
+        return $response::json([
+            "error" => false,
+            "success" => true,
+            "message" => $bookServ
+        ], 201);
+    }
+}
