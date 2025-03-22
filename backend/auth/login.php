6032b5dc9aa34e25bade3a784c03aa96979c23df
rodrigo-backend <rodrigopereirfaria9@gmail.com>
2025-03-22 19:28:27 -0300

    refactor: Organizar estrutura de diretórios do backend

--- backend/auth/login.php
+++ backend/auth/login.php
@@ -4,8 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once  __DIR__ . '/../utils.php';
-require_once __DIR__ . '/../connection.php';
+require_once  __DIR__ . '/../utils/utils.php';
+require_once __DIR__ . '/../database/connection.php';
 
 $data = getData('login');
 
--- backend/database/connection.php
+++ backend/database/connection.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/./utils.php';
-require_once __DIR__ . '/./config.db.php';
+require_once __DIR__ . '/../utils/utils.php';
+require_once __DIR__ . '/./config.php';
 
 // como usar namespace para chamar funções?
 // use function utils\printData;
